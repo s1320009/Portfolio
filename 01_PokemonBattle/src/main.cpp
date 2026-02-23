914b8c4ff67fb3bd73027fe30b33ae431641fe94
MakotoKikuchi <anub6534@gmail.com>
2026-02-23 22:06:22 +0900

    Initial commit (ソースコードのみ)

---
+++ 01_PokemonBattle/src/main.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <string>
+
+class Pokemon {
+public:
+    std::string name;
+    int hp;
+
+    Pokemon(std::string n, int h) : name(n), hp(h) {}
+
+    void greet() {
+        std::cout << name << " が あらわれた！ (HP: " << hp << ")" << std::endl;
+    }
+};
+
+int main() {
+    Pokemon myPokemon("ピカチュウ", 100);
+    myPokemon.greet();
+
+    // 2匹目
+    Pokemon enemyPokemon("ヒトカゲ", 120);
+    enemyPokemon.greet();
+    return 0;
+}
