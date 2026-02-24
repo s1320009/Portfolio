41c5f57e7b06fb6bd1fc2d859e07fd629f5adabd
MakotoKikuchi <anub6534@gmail.com>
2026-02-24 23:03:20 +0900

    バトルループの実装

--- 01_PokemonBattle/src/main.cpp
+++ 01_PokemonBattle/src/main.cpp
@@ -27,15 +27,14 @@ public:
         std::cout << target.name << " に " << selectedMove.power << " の ダメージ!" << std::endl;
     }
 
-    void showMoves() {
-        std::cout << name << " の 技" << std::endl;
-        for (int i = 0; i < (int)moves.size(); i++) {
-            std::cout << i + 1 << ": " << moves[i].name << " (威力: " << moves[i].power << " )" << std::endl;
-        }
+    bool isFainted() {  // 倒れたかの判定
+        return hp <= 0;
     }
+
 };
 
 int main() {
+    // ポケモンの準備
     Pokemon p1("ピカチュウ", 100);
 
     // 技を覚えさせる
@@ -43,23 +42,46 @@ int main() {
     p1.learnMove("でんこうせっか", 40);
 
     Pokemon p2("ヒトカゲ", 100);
+    p2.learnMove("ひのこ", 40);
 
     std::cout << p2.name << " が あらわれた!" << std::endl;
 
-    // -----ここから入力処理　-----
-    p1.showMoves(); // 技の表示
+    // --- バトルループ ---
+    while (!p1.isFainted() && !p2.isFainted()) {
+        // プレイヤーのターン
+        std::cout << "\n------------------------" << std::endl;
+        std::cout << p1.name << " の HP :" << p1.hp << std::endl;
+        std::cout << p2.name << " の HP :" << p2.hp << std::endl;
+        std::cout << p1.name << " は どうする？ \n";
+        for(int i = 0; i < (int)p1.moves.size(); i++) {
+            std::cout << (i + 1) << ": " << p1.moves[i].name << " (威力)" << p1.moves[i].power << "\n";    
+        }
 
-    int choice;
-    std::cout << p1.name << " は どうする？ ";
-    std::cin >> choice; // キーボード入力を受け取る
+        int choice;
+        std::cin >> choice; // キーボード入力を受け取る
 
-    if (choice >= 1 && choice <= (int)p1.moves.size()) {
+        if (choice >= 1 && choice <= (int)p1.moves.size()) {
         p1.attack(choice - 1, p2); // 選択された技で攻撃
-    } else {
-        std::cout << "しかし うまく きまらなかった!" << std::endl;
+        } else {
+            std::cout << "しかし うまく きまらなかった!" << std::endl;
+        }
+
+        // 相手が倒れたか
+        if(p2.isFainted()) {
+            std::cout << p2.name << " は たおれた！" << std::endl;
+            break;
+        }
+
+        // 相手のターン
+        p2.attack(0, p1); // 自動で０番目の技を使う
+
+        // プレイヤーが倒れたか
+        if(p1.isFainted()) {
+            std::cout << p1.name << " は たおれた！" << std::endl;
+            break;
+        }
     }
 
-    // 相手の残りHPを表示
-    std::cout << p2.name << " の 残りHP: " << p2.hp << std::endl;
+    std::cout << "対戦ありがとうございました！" << std::endl;
     return 0;
 }
